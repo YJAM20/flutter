a640073bddc07dce9ff136e84d5d77b1d5516ced
YAMAN MUHANNA <yajm205@gmail.com>
2025-10-06 20:43:45 +0300

    first commit

---
+++ lib/screens/list_view_builder_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListViewBulderScreen extends StatefulWidget {
+  const ListViewBulderScreen({super.key});
+
+  @override
+  State<ListViewBulderScreen> createState() => _ListViewBulderScreenState();
+}
+
+class _ListViewBulderScreenState extends State<ListViewBulderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
